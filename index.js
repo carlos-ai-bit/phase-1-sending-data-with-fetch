e4fde14717a6ca19942954ceb35f472f78bd3c15
Hoperaisers <abdallahsafari@gmail.com>
2023-04-14 14:27:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const formData = {
+    Name: "Abdallah",
+    Email: "Abdallahsafari731@gmail.com",
+};
+
+function submitData() {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            document.body.innerHTML += `<p> ${data.id}</p>`;
+        })
+        .catch((error) => {
+            alert("Bad Things!");
+            document.body.innerHTML += `<p> ${error.message}</p>`;
+        });
+}
